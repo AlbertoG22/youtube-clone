b601e16c3542c79922172e99624ae8c4e5899749
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-03-13 17:48:00 -0600

    add barrel file for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
-import { Navbar } from './components/Navbar';
-import { Feed } from './components/Feed';
-import { VideoDetail } from './components/VideoDetail';
-import { ChannelDetail } from './components/ChannelDetail';
-import './App.css';
-import { SearchFeed } from './components/SearchFeed';
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
 function App() {
 
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from './ChannelDetail';
+export * from './Feed';
+export * from './Navbar';
+export * from './SearchFeed';
+export * from './VideoDetail';
