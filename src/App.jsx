41831d03486d3d2a86a0f2e3b4fac3d9e8b4a263
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-03-13 17:37:34 -0600

    create app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Box } from '@mui/material'
 
 function App() {
 
   return (
-    <>
-      <div>
-        Hello World
-      </div>
-    </>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   )
 }
 
