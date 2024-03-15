6be402a0d8ebe6e8363a1b2de0580ad8164baf7e
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-03-14 21:45:59 -0600

    Feed component pt.3 (make api request)

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import { Sidebar, Videos } from "./";
+import { fetchFromAPI } from '../utils/FetchFromAPI';
 
 export const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New')
+
+  useEffect(() => {
+    fetchFromAPI(`/search?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -12,7 +19,7 @@ export const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         <Typography
           className="copyright"
           variant="body2"
