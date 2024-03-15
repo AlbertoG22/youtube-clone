6136b107539f39703d5888ccda13a9b514d0cae5
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-03-14 21:24:53 -0600

    Feed component pt.2

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -9,12 +9,27 @@ export const Feed = () => {
         sx={{
           height: { sx: "auto", md: "92vh" },
           borderRight: "1px solid #3d3d3d",
-          px: { sx: 0, md: 2 }
+          px: { sx: 0, md: 2 },
         }}
       >
         <Sidebar />
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
           Copyright 2022 Alberto GM
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>videos</span>
         </Typography>
       </Box>
     </Stack>
