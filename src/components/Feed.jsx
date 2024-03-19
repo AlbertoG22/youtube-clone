40ec2d2879707ded35be800215509c3c7e9054bf
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-03-19 14:45:15 -0600

    Api call changes

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -8,8 +8,8 @@ export const Feed = () => {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`/search?part=snippet&q=${selectedCategory}`)
-      .then((data) => setVideos(data.items))
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items));
   }, [selectedCategory]);
 
   return (
