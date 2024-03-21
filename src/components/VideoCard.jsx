fdd619ae8f921465f67fedd3d5aa5909b0a996be
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-03-20 19:54:43 -0600

    adjustment in VideoCard dimensions

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -13,7 +13,7 @@ export const VideoCard = ({ video: { id: { videoId }, snippet }}) => {
   return (
     <Card
       sx={{
-        width: { md: "320px", xs: "100%" },
+        width: { xs: "100%", sm: '358px', md: "320px" },
         boxShadow: "none",
         borderRadius: 0,
       }}
@@ -22,12 +22,12 @@ export const VideoCard = ({ video: { id: { videoId }, snippet }}) => {
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, height: 180 }}
         />
         <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
           <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
             <Typography variant="subtitle1" fontWeight="bold" color="#fff">
-              {snippet?.title.slice(0, 40) || demoVideoTitle.slice(0, 40)}
+              {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
             </Typography>
           </Link>
           <Link
